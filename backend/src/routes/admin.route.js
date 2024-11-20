4ad3855c5ade31df6466fc62a89b3558bec6207b
Adithyan <adithyans2113@gmail.com>
2024-11-20 19:53:38 +0530

    Created the SignUp api and middleware

---
+++ backend/src/routes/admin.route.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware";
+
+const router = Router();
+
+router.get("/", protectRoute , requireAdmin , createSong)
+
+export default router;
